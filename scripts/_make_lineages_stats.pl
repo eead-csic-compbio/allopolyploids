f2a9b48077e4fb006a1a9c11442264ee5bcc19b4
rsancho <rubenben85@hotmail.com>
2020-04-02 09:48:43 +0200

    updated and generalized script

--- scripts/_make_lineages_stats.pl
+++ scripts/_make_lineages_stats.pl
@@ -1,29 +1,40 @@
 #!/usr/bin/perl -w
 
+use strict;
+use warnings;
+use FindBin '$Bin';
+use lib "$Bin";
+use polyconfig;
+
 # Takes log made by _check_lineages_polyploids.pl and prints TSV stats
 #  
-# # B Contreras-Moreira, R Sancho, EEAD-CSIC & EPS-UNIZAR 2017
+# B Contreras-Moreira, R Sancho, EEAD-CSIC & EPS-UNIZAR 2017-20
 
-my @polyploids = ('Bhyb','Bboi','Bret','Bmex','Brup','Bpho','B422');
-my @CODES = qw( A B C D E F G H I all ); 
+my @polyploids = @polyconfig::polyploids;
+my @CODES = @polyconfig::CODES; 
 
 my ($taxon,$col,$code,%stats);
 
-die "# usage: $0 <log.lineage_codes>\n" if(!$ARGV[0]);
+die "# usage: $0 <lineage_codes.log>\n" if(!$ARGV[0]);
 
 # read FASTA sequences to get their length
 open(LOG,"<",$ARGV[0]);
-while(<LOG>)
+while(my $line = <LOG>)
 {
-	#	A	B	C	D	E	F	G	H	I	all
-	#Bhyb	2	0	0	0	0	0	0	0	0	2
-	if(/^(B\S+)/){ 
-		$taxon = $1;
-		chomp; 
-		my @data = split(/\t/,$_);
-		shift(@data);
-		foreach $col (0 .. $#data){
-			$stats{$taxon}{$CODES[$col]} += $data[$col];
+	#        A       B       C       D       E       F       G       H       I       all
+	#Taes    3       1       30      4       1       40      5       5       31      120
+	#Ttur    2       1       17      2       1       0       4       7       24      58
+
+	# loop polyploid taxons and match current line	
+	foreach $taxon (@polyploids){
+		if($line =~ /^$taxon/){
+			chomp($line);; 
+			my @data = split(/\t/,$line);
+			shift(@data);
+			foreach $col (0 .. $#data){
+				$stats{$taxon}{$CODES[$col]} += $data[$col];
+			}
+			last;
 		} 
 	}	
 }
